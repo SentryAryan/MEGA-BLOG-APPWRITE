fa46d1427430dcf6efc9efc0378606c403b1b9a2
SentryAryan <aryansri20011967@gmail.com>
2024-09-28 17:10:10 +0530

    "Updated Login component with new input fields and error handling"

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,21 +1,9 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import Loader from "./Loader";
+import React from 'react'
 
-function ProtectedRoute({ children, authentication = true }) {
-    const navigate = useNavigate();
-    const authStatus = useSelector((state) => state.auth.status);
-    const [loading, setLoading] = React.useState(true);
-
-    useEffect(() => {
-        if (authentication !== authStatus) {
-            navigate(authentication ? "/login" : "/");
-        }
-        setLoading(false);
-    }, [authStatus, navigate, authentication]);
-
-    return loading ? <Loader /> : <>{children}</>;
+const AuthLayout = () => {
+  return (
+    <div>AuthLayout</div>
+  )
 }
 
-export default ProtectedRoute;
+export default AuthLayout
