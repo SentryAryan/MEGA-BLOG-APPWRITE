368dbb4a1f513f4f37538c373791c696eb0a71f6
SentryAryan <aryansri20011967@gmail.com>
2024-09-28 18:22:58 +0530

    changes for login page

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Loader from "./Loader";
 
-const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
+function ProtectedRoute({ children, authentication = true }) {
+    const navigate = useNavigate();
+    const authStatus = useSelector((state) => state.auth.status);
+    const [loading, setLoading] = React.useState(true);
+
+    useEffect(() => {
+        if (authentication !== authStatus) {
+            navigate(authentication ? "/login" : "/");
+        }
+        setLoading(false);
+    }, [authStatus, navigate, authentication]);
+
+    return loading ? <Loader /> : <>{children}</>;
 }
 
-export default AuthLayout
+export default ProtectedRoute;
